5cbcc957bc9de3b17ab880fdea77b708a6de3da9
Nityanand Singh <nns.nitya@gmail.com>
2026-09-18 13:32:23 +0530

    Initial buyer-event ms2

---
+++ services/rea-buyer-event-service/src/main/java/com/buyer/event/ReaBuyerEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buyer.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaBuyerEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaBuyerEventServiceApplication.class, args);
+	}
+
+}
