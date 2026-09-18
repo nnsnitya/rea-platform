c4c67682b58ec5ebb248e93274a3b7ad96518237
Nityanand Singh <nns.nitya@gmail.com>
2026-09-18 17:03:39 +0530

    Add rea-model feature

---
+++ libraries/rea-model/src/main/java/com/rea/ReaModelApplication.java
@@ -0,0 +1,13 @@
+package com.rea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaModelApplication.class, args);
+	}
+
+}
---
+++ libraries/rea-model/src/test/java/com/rea/model/ReaModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rea.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
